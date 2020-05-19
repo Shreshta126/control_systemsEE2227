004928d735908af3c794dd0305fffd802757a39d
Shreshta126 <47571658+Shreshta126@users.noreply.github.com>
2020-05-19 13:01:51 +0530

    Update ee18btech11041_spice.py

--- feedback2/codes/spice/ee18btech11041_spice.py
+++ feedback2/codes/spice/ee18btech11041_spice.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#if using termux 
+
+import subprocess
+import shlex
+#end if
+
 data=np.loadtxt('plotdata.data')  
 
 data2=np.loadtxt('plotdata2.data')  
@@ -16,12 +23,26 @@ plt.xlabel('frequency Hz')
 plt.grid()
 
 
+#if using termux
+plt.savefig('./figs/H1.pdf')
+plt.savefig('./figs/H1.eps')
+subprocess.run(shlex.split("termux-open ./figs/H1.pdf"))
+#else
+
+
 plt.figure(2)
 plt.title ('G from spice simulation')
 plt.plot(data2[:,0],data2[:,1])
 plt.ylabel('G')
 plt.xlabel('frequency Hz')
 plt.grid()
+
+
+#if using termux
+plt.savefig('./figs/G1.pdf')
+plt.savefig('./figs/G1.eps')
+subprocess.run(shlex.split("termux-open ./figs/G1.pdf"))
+#else
 
 
 plt.figure(3)
@@ -31,5 +52,11 @@ plt.ylabel('T')
 plt.xlabel('frequency Hz')
 plt.grid()
 
-plt.show()
 
+#if using termux
+plt.savefig('./figs/T1.pdf')
+plt.savefig('./figs/T1.eps')
+subprocess.run(shlex.split("termux-open ./figs/T1.pdf"))
+#else
+
+#plt.show()
