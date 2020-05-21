9ed9db21ecb508b63dea207d269bf80496454c8d
Shreshta126 <47571658+Shreshta126@users.noreply.github.com>
2020-05-21 11:08:28 +0530

    Update ee18btech11041_spice.py

--- feedback2/codes/spice/ee18btech11041_spice.py
+++ feedback2/codes/spice/ee18btech11041_spice.py
@@ -14,7 +14,7 @@ data2=np.loadtxt('plotdata2.dat')
 
 data3=np.loadtxt('plotdata3.dat')  
 
-
+#plot of H
 plt.figure(1)
 plt.title ('H from spice simulation')
 plt.plot(data[:,0],data[:,1])
@@ -30,7 +30,7 @@ plt.savefig('./figs/H1.eps')
 subprocess.run(shlex.split("termux-open ./figs/H1.pdf"))
 #else
 
-
+#plot of G
 plt.figure(2)
 plt.title ('G from spice simulation')
 plt.plot(data2[:,0],data2[:,1])
@@ -46,7 +46,7 @@ plt.savefig('./figs/G1.eps')
 subprocess.run(shlex.split("termux-open ./figs/G1.pdf"))
 #else
 
-
+#plot of T
 plt.figure(3)
 plt.title ('T from spice simulation')
 plt.plot(data3[:,0],data3[:,1])
